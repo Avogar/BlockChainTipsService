fa3b5bdc50df158f31920b5866965f9b0af71d84
avogar <pav.cruglov@yandex.ru>
2022-03-20 16:52:17 +0100

    Fix

--- src/tips_service/tips_service.py
+++ src/tips_service/tips_service.py
@@ -23,12 +23,12 @@ class TipsService:
         self._sign_and_send_tx(tx, private_key)
 
     def send_tips_to_organization(self, amount: float, organization_name: str, action_id: str, address: str, private_key: str, review=""):
-        tx_properties = self._get_tx_properties(address)
+        tx_properties = self._get_tx_properties(address, value=amount)
         tx = self.contract.functions.sendTipsToOrganization(organization_name, review, action_id).buildTransaction(tx_properties)
         self._sign_and_send_tx(tx, private_key)
 
     def send_tips_to_employee(self, amount: float, organization_name: str, employee_name: str, action_id: str, address: str, private_key: str, review=""):
-        tx_properties = self._get_tx_properties(address)
+        tx_properties = self._get_tx_properties(address, value=amount)
         tx = self.contract.functions.sendTipsToEmployee(organization_name, employee_name, review, action_id).buildTransaction(tx_properties)
         self._sign_and_send_tx(tx, private_key)
 
@@ -64,10 +64,10 @@ class TipsService:
     def get_all_employees(self, organization_name: str):
         return self.contract.functions.getAllEmployeeInOrganization(organization_name).call()
 
-    def _get_tx_properties(self, address: str, value=0):
+    def _get_tx_properties(self, address: str, value=0.):
         nonce = self.web3.eth.getTransactionCount(address)
         return {'gas': 600000, 'gasPrice': self.web3.toWei('10', 'gwei'), 'from': address, 'nonce': nonce,
-                'value': self.web3.toWei(float(value), "ether")}
+                'value': self.web3.toWei(value, "ether")}
 
     def _sign_and_send_tx(self, tx, private_key: str):
         signed_txn = self.web3.eth.account.signTransaction(tx, private_key=private_key)
